4a3061e5f27e2e5ae9973ca3aacd09abd5aa176f
Thomas Monte <thomasmont3@gmail.com>
2020-06-24 03:38:55 +0000

    change bash script into replit config

--- run.sh
+++
@@ -1,1 +0,0 @@
-fsharpc --nologo -r Newtonsoft.Json.dll -r System.Security.Permissions.dll -r System.Configuration.ConfigurationManager.dll -r System.Collections.Immutable.dll -r Akka.dll --out:main.exe MyActor.fs main.fs && mono main.exe
